bcbb14380e154fba765296ec2e1c4f8634d8fd97
alux444 <aluux999@gmail.com>
2023-04-14 15:57:50 +1200

    project array mapping

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,25 +2,39 @@ import React from 'react'
 import './projects.css'
 import PROJECT1 from '../../img/website.png'
 
+const data = [
+  {
+    id:1,
+    image: PROJECT1,
+    title: 'Portfolio Website Project',
+    github: 'https://github.com/alux444/react-portfolio-project'
+  }
+]
+
 const Projects = () => {
   return (
     <section id="projects">
       <h5>My Current</h5>
-      <h2>Projectss</h2>
+      <h2>Projects</h2>
 
     <div className='container portfolio_container'>
-      <article className='portfolio_item'>
-        <div className='portfolio_item-image'>
-          <img src={PROJECT1} alt="websiteproj"></img>
-        </div>
-        <h3>Portfolio Website Project</h3>
-        <div className='portfolio_item-cta'>
-          <a href="https://github.com/alux444/react-portfolio-project" target="_blank" rel="noreferrer" className='btn'>
-           GitHub
-          </a>
-        </div>
-
+      {
+        data.map(({id,image,title,github}) => {
+          return (
+            <article key={id} className='portfolio_item'>
+             <div className='portfolio_item-image'>
+               <img src={image} alt="websiteproj"></img>
+            </div>
+            <h3>{title}</h3>
+              <div className='portfolio_item-cta'>
+                <a href={github} target="_blank" rel="noreferrer" className='btn'>
+               GitHub
+               </a>
+             </div>
       </article>
+          )
+        })
+      }
 
       <article className='portfolio_item'>
         <div className='portfolio_item-image'>
