1c77ff2998c4d2bce855992303c9aad64205a568
alux444 <aluux999@gmail.com>
2023-04-14 22:29:21 +1200

    footer finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,17 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <ul className='permalinks'>
+        <li><a href="#">Back To Top</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact Me</a></li>
+      </ul>
+
+      <a className='footer-copyright'>2023 &copy; alux444</a>
+    </footer>
   )
 }
 
