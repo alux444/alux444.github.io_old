3970a4e5cf428df3da4721771389f5a87f763a26
alux444 <aluux999@gmail.com>
2023-04-14 17:39:23 +1200

    revamp of about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './about.css'
-import ME from '../../img/about_me.png'
 import {DiJava} from 'react-icons/di'
 import {DiJavascript1} from 'react-icons/di'
 import {AiFillHtml5} from 'react-icons/ai'
@@ -12,45 +11,38 @@ import {FaReact} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <section id="about">
-      <h5>Get to know</h5>
-      <h2>About Me</h2>
-
+    <section id='about'>
+      <h5>About Me</h5>
       <div className='container about_container'>
-        <div className='about_me'>
-          <div className='about_me-image'>
-            <img src={ME} alt="About"/>
-          </div>
-        </div>
-
-      <div className='about_content'>
-        <div className='about_cards'>
-          <article className='about_card'>
-            <h5>Languages</h5>
-            <h6>Java <DiJava className="about_icons"/></h6>
-            <h6>JavaScript <DiJavascript1 className="about_icons"/></h6>
-            <h6>C <SiC className="about_icons"/></h6>
-            <h6>HTML <AiFillHtml5 className="about_icons"/></h6>
-            <h6>CSS <DiCss3 className="about_icons"/></h6>
-            <h6>MATLAB</h6>
-          </article>
-
-          <article className='about_card'>
-            <h5>Frameworks</h5>
-            <h6>React <FaReact className="about_icons"/></h6>
-          </article>
-
-          <article className='about_card'>
-            <h5>Tools</h5>
-            <h6>Git <AiFillGithub className="about_icons"/></h6>
-            <h6>GitHub <AiFillGithub className="about_icons"/></h6>
-          </article>
-
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptates consectetur officia eius nesciunt asperiores illo exercitationem! Non sed excepturi, error eaque in vel eligendi iure blanditiis deserunt nihil consequuntur?</p>
-
-          <a href="#contact" className='btn btn-primary'>Contact Me!</a>
-        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, similique officiis. Mollitia minus esse suscipit distinctio tempore molestias consequuntur quas.</p>
       </div>
+      <h2>My Skills</h2>
+      <div className='containter about_container'>
+        <div className='about_languages'>
+          <h3>Languages</h3>
+          <div className='about_content'>
+            <article className='about_details'>
+              <h4><DiJava className="about_icons"/>Java</h4>
+              <h4><DiJavascript1 className="about_icons"/>JavaScript</h4>
+              <h4><SiC className="about_icons"/>C</h4>
+              <h4><AiFillHtml5 className="about_icons"/>HTML</h4>
+              <h4><DiCss3 className="about_icons"/>CSS</h4>
+              <h4>MATLAB</h4>
+            </article>
+          </div>
+
+        </div>
+        <div className='about_tools'>
+          <h3>Tools and Frameworks</h3>
+          <div className='about_content'>
+            <article className='about_details'>
+            <h6><FaReact className="about_icons"/>React</h6>
+            <h6><AiFillGithub className="about_icons"/>Git</h6>
+            <h6><AiFillGithub className="about_icons"/>Github</h6>
+            </article>
+          </div>
+
+        </div>
       </div>
     </section>
   )
