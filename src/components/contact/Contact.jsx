8454843698fed6ebfac76313f72f3a2b78057201
alux444 <aluux999@gmail.com>
2023-04-14 22:17:00 +1200

    media query fixes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './contact.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {TfiEmail} from 'react-icons/tfi'
 
 const Contact = () => {
   return (
     <section id="contact">
-      Contact
+      <h2>Contact Me</h2>
+      
+      <div className='container contact_container'>
+        <div className='contact_options'>
+          <article className='contact_option'>
+            <h4>Email <TfiEmail/></h4>
+            <h5>aluux999@gmail.com</h5>
+            <a href="mailto:aluux999@gmail.com">Send an email</a>
+          </article>
+
+          <article className='contact_option'>
+            <h4>LinkedIn <BsLinkedin/></h4>
+            <h5>Contact me on LinkedIn</h5>
+            <a href="https://www.linkedin.com/in/alex-liang-7b25a6269/" target='_blank' rel="noreferrer">My LinkedIn</a>
+          </article>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,25 +7,26 @@ const data = [
     id:1,
     image: PROJECT1,
     title: 'Portfolio Website Project',
-    github: 'https://github.com/alux444/react-portfolio-project'
+    github: 'https://github.com/alux444/react-portfolio-project',
+    description: 'Skills : React, JavaScript, CSS, HTML'
   }
 ]
 
 const Projects = () => {
   return (
     <section id="projects">
-      <h5>My Current</h5>
       <h2>Projects</h2>
 
     <div className='container portfolio_container'>
       {
-        data.map(({id,image,title,github}) => {
+        data.map(({id,image,title,github,description}) => {
           return (
             <article key={id} className='portfolio_item'>
              <div className='portfolio_item-image'>
                <img src={image} alt="websiteproj"></img>
             </div>
             <h3>{title}</h3>
+            <p>{description}</p>
               <div className='portfolio_item-cta'>
                 <a href={github} target="_blank" rel="noreferrer" className='btn'>
                GitHub
