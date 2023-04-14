547c38f2cde1b02d205c130eb112c15bee873b66
alux444 <aluux999@gmail.com>
2023-04-14 13:31:05 +1200

    header logic + call to action buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../img/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="containter header_container">
+        <h5>Hey, I'm</h5>
+        <h1>Alex Liang</h1>
+        <h5 className = "text-light">Part II Software Engineer</h5>
+        <h5 className = "text-light">at the University of Auckland</h5>
+        <CTA></CTA>
+
+        <div className="me">
+          <img src={ME} alt="me"></img> 
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header-socials'>
+        <a href='#' target='_blank'>LinkedIn</a>
+        <a href='#' target='_blank'>Github</a>
+        <a href='#' target='_blank'>Email</a>
+    </div>
+  )
+}
+
+export default HeaderSocials
