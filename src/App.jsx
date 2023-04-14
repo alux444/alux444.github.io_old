d0c28868a3a1256d62956f17af385f4d2c37deaf
alux444 <aluux999@gmail.com>
2023-04-14 12:54:23 +1200

    added all components for site

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Projects from './components/projects/Projects'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
