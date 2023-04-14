8dea888d000f33ffde512008e2a12fb27156e423
alux444 <aluux999@gmail.com>
2023-04-14 15:41:01 +1200

    project section styling + contents

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './projects.css'
+import PROJECT1 from '../../img/website.png'
 
 const Projects = () => {
   return (
     <section id="projects">
-      Projects
+      <h5>My Current</h5>
+      <h2>Projectss</h2>
+
+    <div className='container portfolio_container'>
+      <article className='portfolio_item'>
+        <div className='portfolio_item-image'>
+          <img src={PROJECT1} alt="websiteproj"></img>
+        </div>
+        <h3>Portfolio Website Project</h3>
+        <div className='portfolio_item-cta'>
+          <a href="https://github.com/alux444/react-portfolio-project" target="_blank" rel="noreferrer" className='btn'>
+           GitHub
+          </a>
+        </div>
+
+      </article>
+
+      <article className='portfolio_item'>
+        <div className='portfolio_item-image'>
+        </div>
+        <h4>More TBA</h4>
+      </article>
+    </div>
     </section>
   )
 }
