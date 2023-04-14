f25de425e0fa9f9a049b7ce7cb06d207688a800c
alux444 <aluux999@gmail.com>
2023-04-14 14:19:57 +1200

    navbar active style changes using useState hook

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,21 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBookAlt} from 'react-icons/bi'
 import {AiOutlineGithub} from 'react-icons/ai'
 import {BiMessageDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBookAlt/></a>
-      <a href="#projects"><AiOutlineGithub/></a>
-      <a href="#contact"><BiMessageDetail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
+
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''} ><AiOutlineUser/></a>
+
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav ==='#experience' ? 'active' : ''}><BiBookAlt/></a>
+
+      <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav ==='#projects' ? 'active' : ''}><AiOutlineGithub/></a>
+
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ==='#contact' ? 'active' : ''}><BiMessageDetail/></a>
 
     </nav>
   )
