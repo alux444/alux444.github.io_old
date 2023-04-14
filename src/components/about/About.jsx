70f658d3f30227ca149b40fbb4b481956d152a06
alux444 <aluux999@gmail.com>
2023-04-14 15:01:31 +1200

    further stylistic changes for about and bugfixing

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../img/me.png'
+import ME from '../../img/about_me.png'
 import {DiJava} from 'react-icons/di'
 import {DiJavascript1} from 'react-icons/di'
 import {AiFillHtml5} from 'react-icons/ai'
@@ -23,30 +23,30 @@ const About = () => {
           </div>
         </div>
 
-      <div className='about_content'>
+      <div>
         <div className='about_cards'>
           <article className='about_card'>
             <h5>Languages</h5>
-            <p>Java <DiJava/></p>
-            <p>JavaScript <DiJavascript1/></p>
-            <p>C <SiC/></p>
-            <p>HTML <AiFillHtml5/></p>
-            <p>CSS <DiCss3/></p>
+            <p>Java <DiJava className="about_icons"/></p>
+            <p>JavaScript <DiJavascript1 className="about_icons"/></p>
+            <p>C <SiC className="about_icons"/></p>
+            <p>HTML <AiFillHtml5 className="about_icons"/></p>
+            <p>CSS <DiCss3 className="about_icons"/></p>
             <p>MATLAB</p>
           </article>
 
           <article className='about_card'>
             <h5>Frameworks</h5>
-            <p>React <FaReact/></p>
+            <p>React <FaReact className="about_icons"/></p>
           </article>
 
           <article className='about_card'>
             <h5>Tools</h5>
-            <p>Git <AiFillGithub/></p>
-            <p>GitHub <AiFillGithub/></p>
+            <p>Git <AiFillGithub className="about_icons"/></p>
+            <p>GitHub <AiFillGithub className="about_icons"/></p>
           </article>
 
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptates consectetur officia eius nesciunt asperiores illo exercitationem! Non sed excepturi, error eaque in vel eligendi iure blanditiis deserunt nihil consequuntur?</p>
+          <p className='about_content'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptates consectetur officia eius nesciunt asperiores illo exercitationem! Non sed excepturi, error eaque in vel eligendi iure blanditiis deserunt nihil consequuntur?</p>
 
           <a href="#contact" className='btn btn-primary'>Contact Me!</a>
         </div>
