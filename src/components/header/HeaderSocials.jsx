795c761034be200e4fb66e18834c0d92d6c3e41a
alux444 <aluux999@gmail.com>
2023-04-14 23:44:37 +1200

    fixing hyperlinks

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {AiFillGithub} from 'react-icons/ai'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/alex-liang-7b25a6269/' target='_blank'><h1><BsLinkedin/></h1></a>
-        <a href='https://github.com/alux444' target='_blank'><h1><AiFillGithub/></h1></a>
-        <a href='#' target='_blank'><h1><TfiEmail/></h1></a>
+        <a href='https://www.linkedin.com/in/alex-liang-7b25a6269/' rel='noreferrer' target='_blank'><h1><BsLinkedin/></h1></a>
+        <a href='https://github.com/alux444' rel='noreferrer' target='_blank'><h1><AiFillGithub/></h1></a>
+        <a href="mailto:aluux999@gmail.com" rel='noreferrer' target='_blank'><h1><TfiEmail/></h1></a>
     </div>
   )
 }
