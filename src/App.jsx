d22247ba6e972c6c6b03f9e621b93975d0fd327f
alux444 <aluux999@gmail.com>
2023-04-14 15:17:38 +1200

    removed irrelevant section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
-import Experience from './components/experience/Experience'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
@@ -13,7 +12,6 @@ export const App = () => {
       <Header />
       <Nav />
       <About />
-      <Experience />
       <Projects />
       <Contact />
       <Footer />
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
-import {BiBookAlt} from 'react-icons/bi'
 import {AiOutlineGithub} from 'react-icons/ai'
 import {BiMessageDetail} from 'react-icons/bi'
 import {useState} from 'react'
@@ -14,8 +13,6 @@ const Nav = () => {
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav ==='#' ? 'active' : ''}><AiOutlineHome/></a>
 
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''} ><AiOutlineUser/></a>
-
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav ==='#experience' ? 'active' : ''}><BiBookAlt/></a>
 
       <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav ==='#projects' ? 'active' : ''}><AiOutlineGithub/></a>
 
