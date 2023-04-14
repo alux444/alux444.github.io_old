9a4e57671f09d26630ebfa4acbb8aaf65960c723
alux444 <aluux999@gmail.com>
2023-04-14 13:59:01 +1200

    header stylistic changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../img/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className="containter header_container">
+      <div className="container header_container">
         <h5>Hey, I'm</h5>
         <h1>Alex Liang</h1>
         <h5 className = "text-light">Part II Software Engineer</h5>
         <h5 className = "text-light">at the University of Auckland</h5>
         <CTA></CTA>
+        <HeaderSocials/>
 
         <div className="me">
           <img src={ME} alt="me"></img> 
         </div>
 
         <a href="#contact" className='scroll_down'>Scroll Down</a>
-        
+
       </div>
     </header>
   )
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {TfiEmail} from 'react-icons/tfi'
+import {AiFillGithub} from 'react-icons/ai'
 
 const HeaderSocials = () => {
   return (
-    <div className='header-socials'>
-        <a href='#' target='_blank'>LinkedIn</a>
-        <a href='#' target='_blank'>Github</a>
-        <a href='#' target='_blank'>Email</a>
+    <div className='header_socials'>
+        <a href='https://www.linkedin.com/in/alex-liang-7b25a6269/' target='_blank'><BsLinkedin/></a>
+        <a href='https://github.com/alux444' target='_blank'><AiFillGithub/></a>
+        <a href='#' target='_blank'><TfiEmail/></a>
     </div>
   )
 }
