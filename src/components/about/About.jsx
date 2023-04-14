cd7c34235fa0c748a2b5ba85e60bc8c8c4d7a540
alux444 <aluux999@gmail.com>
2023-04-14 17:58:56 +1200

    styling of about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,8 +12,8 @@ import {FaReact} from 'react-icons/fa'
 const About = () => {
   return (
     <section id='about'>
-      <h5>About Me</h5>
-      <div className='container about_container'>
+      <h2>About Me</h2>
+      <div className='container about_paragraph'>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, similique officiis. Mollitia minus esse suscipit distinctio tempore molestias consequuntur quas.</p>
       </div>
       <h2>My Skills</h2>
@@ -22,11 +22,11 @@ const About = () => {
           <h3>Languages</h3>
           <div className='about_content'>
             <article className='about_details'>
-              <h4><DiJava className="about_icons"/>Java</h4>
-              <h4><DiJavascript1 className="about_icons"/>JavaScript</h4>
-              <h4><SiC className="about_icons"/>C</h4>
-              <h4><AiFillHtml5 className="about_icons"/>HTML</h4>
-              <h4><DiCss3 className="about_icons"/>CSS</h4>
+              <h4><DiJava className="about_icons"/> Java</h4>
+              <h4><DiJavascript1 className="about_icons"/> JavaScript</h4>
+              <h4><SiC className="about_icons"/> C</h4>
+              <h4><AiFillHtml5 className="about_icons"/> HTML</h4>
+              <h4><DiCss3 className="about_icons"/> CSS</h4>
               <h4>MATLAB</h4>
             </article>
           </div>
@@ -36,9 +36,9 @@ const About = () => {
           <h3>Tools and Frameworks</h3>
           <div className='about_content'>
             <article className='about_details'>
-            <h6><FaReact className="about_icons"/>React</h6>
-            <h6><AiFillGithub className="about_icons"/>Git</h6>
-            <h6><AiFillGithub className="about_icons"/>Github</h6>
+            <h4><FaReact className="about_icons"/> React</h4>
+            <h4><AiFillGithub className="about_icons"/> Git</h4>
+            <h4><AiFillGithub className="about_icons"/> Github</h4>
             </article>
           </div>
 
