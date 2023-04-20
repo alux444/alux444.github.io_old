c0ba2ffbd9a8bec37eabfe02ba6de6ef47844833
alux444 <aluux999@gmail.com>
2023-04-20 13:38:11 +1200

    weather app update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './projects.css'
 import PROJECT1 from '../../img/website.png'
+import PROJECT2 from '../../img/weatherApp.png'
 
 const data = [
   {
@@ -8,7 +9,19 @@ const data = [
     image: PROJECT1,
     title: 'Portfolio Website Project',
     github: 'https://github.com/alux444/react-portfolio-project',
-    description: 'Skills : React, JavaScript, CSS, HTML'
+    description: 'A simple react website created to display my portfolio',
+    skills: 'Skills : React, JavaScript, CSS, HTML',
+    link: 'https://alux444.github.io/'
+  },
+
+  {
+    id:2,
+    image: PROJECT2,
+    title: 'Weather API Project',
+    github: 'https://github.com/alux444/weather-api-project',
+    description: 'A simple react weather app which calls on API information from weatherAPI.com',
+    skills: 'Skills : React, JavaScript, CSS, HTML, RapidAPI/WeatherAPI.com',
+    link: 'https://alux444.github.io/weather-api-project/'
   }
 ]
 
@@ -19,7 +32,7 @@ const Projects = () => {
 
     <div className='container portfolio_container'>
       {
-        data.map(({id,image,title,github,description}) => {
+        data.map(({id,image,title,github,skills,description,link}) => {
           return (
             <article key={id} className='portfolio_item'>
              <div className='portfolio_item-image'>
@@ -27,9 +40,13 @@ const Projects = () => {
             </div>
             <h3>{title}</h3>
             <p>{description}</p>
+            <p>{skills}</p>
               <div className='portfolio_item-cta'>
                 <a href={github} target="_blank" rel="noreferrer" className='btn'>
                GitHub
+               </a>
+               <a href={link} target="_blank" rel="noreferrer" className='btn'>
+               Link
                </a>
              </div>
       </article>
