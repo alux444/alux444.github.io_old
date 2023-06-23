527275d6a4cbffa6cb16244eb6af2ea5b0dd6089
alux444 <aluux999@gmail.com>
2023-06-23 16:13:31 +1200

    styling improvements

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,9 @@ import { AiFillGithub } from "react-icons/ai";
 import { FaReact } from "react-icons/fa";
 import { SiMui } from "react-icons/si";
 import { SiPostgresql } from "react-icons/si";
-import { SiRedis } from "react-icons/si";
+import { SiTrello } from "react-icons/si";
+import { BsFiletypeSql } from "react-icons/bs";
+import { SiGithubactions } from "react-icons/si";
 import { SiExpress } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiFirebase } from "react-icons/si";
@@ -38,60 +40,83 @@ const About = () => {
         <p>You can check out some of my recent projects down below.</p>
       </div>
       <h2>My Skills</h2>
-      <div className="container about_container">
+
+      <div
+        className="container about_container"
+        style={{
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
         <div className="about_languages">
           <h3>Languages</h3>
           <div className="about_content">
             <article className="about_details">
               <h4>
-                <DiJava className="about_icons" /> Java
+                <DiJava className="about_icons" /> Java |
               </h4>
               <h4>
-                <DiJavascript1 className="about_icons" /> JavaScript
+                <DiJavascript1 className="about_icons" /> JavaScript |
               </h4>
               <h4>
-                <SiPostgresql className="about_icons" /> SQL (PostgreSQL)
+                <BsFiletypeSql className="about_icons" /> SQL |
               </h4>
               <h4>
-                <SiC className="about_icons" /> C
+                <SiC className="about_icons" /> C |
               </h4>
               <h4>
-                <AiFillHtml5 className="about_icons" /> HTML
+                <AiFillHtml5 className="about_icons" /> HTML |
               </h4>
               <h4>
-                <DiCss3 className="about_icons" /> CSS
+                <DiCss3 className="about_icons" /> CSS |
               </h4>
               <h4>MATLAB</h4>
             </article>
           </div>
         </div>
+
         <div className="about_tools">
           <h3>Tools and Frameworks</h3>
           <div className="about_content">
             <article className="about_details">
               <h4>
-                <FaReact className="about_icons" /> React
+                <FaReact className="about_icons" /> React |
               </h4>
               <h4>
-                <AiFillGithub className="about_icons" /> Git
+                <SiMui classname="about_icons" /> MaterialsUI |
               </h4>
               <h4>
-                <AiFillGithub className="about_icons" /> Github
+                <SiTailwindcss classname="about_icons" /> TailwindCSS |
               </h4>
               <h4>
-                <SiMui classname="about_icons" /> MaterialsUI
+                <SiFirebase className="about_icons" /> Firebase |
               </h4>
               <h4>
-                <SiTailwindcss classname="about_icons" /> TailwindCSS
-              </h4>
-              <h4>
-                <SiFirebase className="about_icons" /> Firebase
-              </h4>
-              <h4>
-                <SiRedis classname="about_icons" /> Redis DB
+                <SiPostgresql classname="about_icons" /> PostgreSQL |
               </h4>
               <h4>
                 <SiExpress classname="about_icons" /> ExpressJS
+              </h4>
+            </article>
+          </div>
+        </div>
+
+        <div className="about_other">
+          <h3>Other</h3>
+          <div className="about_content">
+            <article className="about_details">
+              <h4>CI/CD Practice | </h4>
+              <h4>
+                <AiFillGithub className="about_icons" /> Git / GitHub |
+              </h4>
+              <h4>
+                <SiGithubactions className="about_icons" /> Github Actions |
+              </h4>
+              <h4>
+                <SiTrello className="about_icons" />
+                Trello
               </h4>
             </article>
           </div>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import './contact.css'
-import {BsLinkedin} from 'react-icons/bs'
-import {TfiEmail} from 'react-icons/tfi'
+import React from "react";
+import "./contact.css";
+import { BsLinkedin } from "react-icons/bs";
+import { TfiEmail } from "react-icons/tfi";
 
 const Contact = () => {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
-      
-      <div className='container contact_container'>
-        <div className='contact_options'>
-          <article className='contact_option'>
-            <h4>Email <TfiEmail/></h4>
-            <h5>aluux999@gmail.com</h5>
-            <a href="mailto:aluux999@gmail.com">Send an email</a>
-          </article>
 
-          <article className='contact_option'>
-            <h4>LinkedIn <BsLinkedin/></h4>
-            <h5>Contact me on LinkedIn</h5>
-            <a href="https://www.linkedin.com/in/alex-liang-7b25a6269/" target='_blank' rel="noreferrer">My LinkedIn</a>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <h4>
+              Email <TfiEmail />
+            </h4>
+            <a href="mailto:aluux999@gmail.com">
+              <h5>aluux999@gmail.com</h5>
+            </a>
+            <hr />
+            <h4>
+              LinkedIn <BsLinkedin />
+            </h4>
+            <a
+              href="https://www.linkedin.com/in/alex-liang-7b25a6269/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h5>Contact me on LinkedIn</h5>
+            </a>
           </article>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
