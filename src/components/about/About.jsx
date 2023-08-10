89107227623ed1b6f9f673d966aa60df6d8287c4
alux444 <aluux999@gmail.com>
2023-08-10 16:17:46 +1200

    updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,6 +9,7 @@ import { SiPython } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
 import { SiPostgresql } from "react-icons/si";
 import { BsFiletypeSql } from "react-icons/bs";
+import { SiTypescript } from "react-icons/si";
 import { SiGithubactions } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiFirebase } from "react-icons/si";
@@ -16,103 +17,115 @@ import { TbBrandThreejs } from "react-icons/tb";
 import { SiTensorflow } from "react-icons/si";
 
 const About = () => {
-  return (
-    <section id="about">
-      <h2>About Me</h2>
-      <div className="ab_container about_paragraph item">
-        <p>
-          Hey, I'm Alex. I'm a software engineering student situated in
-          Auckland, New Zealand.
-        </p>
-        <p>
-          I'm on the search for any summer 2023 internship opportunities in New
-          Zealand or Australia.
-        </p>
-        <p>
-          I'm open to any opportunities that will help me grow as a software
-          developer.
-        </p>
-      </div>
-      <h2>My Skills</h2>
+    return (
+        <section id="about">
+            <h2>About Me</h2>
+            <div className="ab_container about_paragraph item">
+                <p>
+                    Hey, I'm Alex. I'm a software engineering student situated
+                    in Auckland, New Zealand.
+                </p>
+                <p>
+                    I'm on the search for any summer 2023 internship
+                    opportunities in New Zealand or Australia.
+                </p>
+                <p>
+                    I'm open to any opportunities that will help me grow as a
+                    software developer.
+                </p>
+            </div>
+            <h2>My Skills</h2>
 
-      <div
-        className="container about_container item"
-        style={{
-          display: "flex",
-          alignContent: "center",
-          justifyContent: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <div className="about_languages delayone item">
-          <h3>Languages</h3>
-          <div className="about_content">
-            <article className="about_details">
-              <h4>
-                <DiJava className="about_icons" /> Java |
-              </h4>
-              <h4>
-                <DiJavascript1 className="about_icons" /> JavaScript |
-              </h4>
-              <h4>
-                <SiPython className="about_icons" /> Python |
-              </h4>
-              <h4>
-                <BsFiletypeSql className="about_icons" /> SQL |
-              </h4>
-              <h4>
-                <AiFillHtml5 className="about_icons" /> HTML |
-              </h4>
-              <h4>
-                <DiCss3 className="about_icons" /> CSS
-              </h4>
-            </article>
-          </div>
-        </div>
+            <div
+                className="container about_container item"
+                style={{
+                    display: "flex",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    flexWrap: "wrap",
+                }}
+            >
+                <div className="about_languages delayone item">
+                    <h3>Languages</h3>
+                    <div className="about_content">
+                        <article className="about_details">
+                            <h4>
+                                <DiJava className="about_icons" /> Java |
+                            </h4>
+                            <h4>
+                                <SiTypescript className="about_icons" />{" "}
+                                TypeScript |
+                            </h4>
+                            <h4>
+                                <DiJavascript1 className="about_icons" />{" "}
+                                JavaScript |
+                            </h4>
+                            <h4>
+                                <SiPython className="about_icons" /> Python |
+                            </h4>
+                            <h4>
+                                <BsFiletypeSql className="about_icons" /> SQL |
+                            </h4>
+                            <h4>
+                                <AiFillHtml5 className="about_icons" /> HTML |
+                            </h4>
+                            <h4>
+                                <DiCss3 className="about_icons" /> CSS
+                            </h4>
+                        </article>
+                    </div>
+                </div>
 
-        <div className="about_tools delaytwo item">
-          <h3>Tools and Frameworks</h3>
-          <div className="about_content">
-            <article className="about_details">
-              <h4>
-                <FaReact className="about_icons" /> React |
-              </h4>
-              <h4>
-                <SiFirebase className="about_icons" /> Firebase |
-              </h4>
-              <h4>
-                <SiPostgresql classname="about_icons" /> PostgreSQL |
-              </h4>
-              <h4>
-                <SiTailwindcss classname="about_icons" /> TailwindCSS |
-              </h4>
-              <h4>
-                <TbBrandThreejs className="about_icons" /> Three.js
-              </h4>
-            </article>
-          </div>
-        </div>
+                <div className="about_tools delaytwo item">
+                    <h3>Tools and Frameworks</h3>
+                    <div className="about_content">
+                        <article className="about_details">
+                            <h4>
+                                <FaReact className="about_icons" /> React |
+                            </h4>
+                            <h4>
+                                <SiFirebase className="about_icons" /> Firebase
+                                |
+                            </h4>
+                            <h4>
+                                <SiPostgresql classname="about_icons" />{" "}
+                                PostgreSQL |
+                            </h4>
+                            <h4>
+                                <SiTailwindcss classname="about_icons" />{" "}
+                                TailwindCSS |
+                            </h4>
+                            <h4>
+                                <TbBrandThreejs className="about_icons" />{" "}
+                                Three.js
+                            </h4>
+                        </article>
+                    </div>
+                </div>
 
-        <div className="about_other delaythree item">
-          <h3>Other</h3>
-          <div className="about_content">
-            <article className="about_details">
-              <h4>CI/CD Practice | </h4>
-              <h4>
-                <AiFillGithub className="about_icons" /> Git / GitHub |
-              </h4>
-              <h4>
-                <SiGithubactions className="about_icons" /> Github Actions |
-              </h4>
-              <h4>
-                <SiTensorflow className="about_icons" /> TensorFlow
-              </h4>
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+                <div className="about_other delaythree item">
+                    <h3>Other</h3>
+                    <div className="about_content">
+                        <article className="about_details">
+                            <h4>CI/CD Practice | </h4>
+                            <h4>
+                                <AiFillGithub className="about_icons" /> Git /
+                                GitHub |
+                            </h4>
+                            <h4>
+                                <SiGithubactions className="about_icons" />{" "}
+                                Github Actions |
+                            </h4>
+                            <h4>
+                                <SiTensorflow className="about_icons" />{" "}
+                                TensorFlow
+                            </h4>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default About;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,170 +5,211 @@ import store from "../../img/store.png";
 import FitnessApp from "../../img/fitnessApp.png";
 import weatherApp2 from "../../img/weatherApp2.png";
 import roguelike from "../../img/roguelike.png";
+import trackTrekker from "../../img/trackTrekker.png";
 
 const Projects = () => {
-  return (
-    <section id="projects">
-      <h2>Projects</h2>
+    return (
+        <section id="projects">
+            <h2>Projects</h2>
 
-      <div className="TOTALCONT container portfolio_container item">
-        <article className="portfolio_item item">
-          <div className="portfolio_item-image">
-            <img src={roguelike} alt="roguelikeGame"></img>
-          </div>
-          <h3>Roguelike Game</h3>
-          <small>
-            A roguelike game built with Python and the tcod library, with
-            randomly generated dungeon levels, enemy npcs and consumables.
-          </small>
-          <br />
-          <p>Skills : Python, tcod</p>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/alux444/roguelike-game"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              GitHub
-            </a>
-          </div>
-        </article>
+            <div className="TOTALCONT container portfolio_container item">
+                <article className="portfolio_item item">
+                    <div className="portfolio_item-image">
+                        <img src={roguelike} alt="roguelikeGame"></img>
+                    </div>
+                    <h3>Roguelike Game</h3>
+                    <small>
+                        A roguelike game built with Python and the tcod library,
+                        with randomly generated dungeon levels, enemy npcs and
+                        consumables.
+                    </small>
+                    <br />
+                    <p>Skills : Python, tcod</p>
+                    <div className="portfolio_item-cta">
+                        <a
+                            href="https://github.com/alux444/roguelike-game"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            GitHub
+                        </a>
+                    </div>
+                </article>
 
-        <article className="portfolio_item item">
-          <div className="portfolio_item-image">
-            <img src={store} alt="store" />
-          </div>
-          <h3>KiwiMart</h3>
-          <small>
-            Online shopping webapp with an admin dashboard allowing for CRUD
-            operations. This project was built in a team of 3, where I acted as
-            a project manager and was responsible for integrating the backend
-            communication to Firebase.
-          </small>
-          <br />
-          <p>Skills : ReactJS, Firebase, JavaScript, TailwindCSS</p>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/alux444/online-store"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://alux444.github.io/online-store/"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              Link
-            </a>
-          </div>
-        </article>
+                <article className="portfolio_item item">
+                    <div className="portfolio_item-image">
+                        <img src={store} alt="store" />
+                    </div>
+                    <h3>KiwiMart</h3>
+                    <small>
+                        Online shopping webapp with an admin dashboard allowing
+                        for CRUD operations. This project was built in a team of
+                        3, where I acted as a project manager and was
+                        responsible for integrating the backend communication to
+                        Firebase.
+                    </small>
+                    <br />
+                    <p>Skills : ReactJS, Firebase, JavaScript, TailwindCSS</p>
+                    <div className="portfolio_item-cta">
+                        <a
+                            href="https://github.com/alux444/online-store"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            href="https://alux444.github.io/online-store/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            Link
+                        </a>
+                    </div>
+                </article>
 
-        <article className="portfolio_item item">
-          <div className="portfolio_item-image">
-            <img src={chatapp} alt="chatapp" />
-          </div>
-          <h3>Firebase Chatapp</h3>
-          <small>
-            This is a live messaging chat app built through React. Firebase is
-            used for storage of messages, images, and users.
-          </small>
-          <br />
-          <p>Skills : ReactJS, Firebase, JavaScript, GitHub Actions (CICD)</p>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/alux444/firebase-chatapp"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://alux444.github.io/firebase-chatapp/"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              Link
-            </a>
-          </div>
-        </article>
+                <article className="portfolio_item item">
+                    <div className="portfolio_item-image">
+                        <img src={chatapp} alt="chatapp" />
+                    </div>
+                    <h3>Firebase Chatapp</h3>
+                    <small>
+                        This is a live messaging chat app built through React.
+                        Firebase is used for storage of messages, images, and
+                        users.
+                    </small>
+                    <br />
+                    <p>
+                        Skills : ReactJS, Firebase, JavaScript, GitHub Actions
+                        (CICD)
+                    </p>
+                    <div className="portfolio_item-cta">
+                        <a
+                            href="https://github.com/alux444/firebase-chatapp"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            href="https://alux444.github.io/firebase-chatapp/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            Link
+                        </a>
+                    </div>
+                </article>
 
-        <article className="portfolio_item item">
-          <div className="portfolio_item-image">
-            <img src={weatherApp2} alt="weather2" />
-          </div>
-          <h3>Weather App V2</h3>
-          <small>
-            This is a revisited version of my first project (weather app). It
-            includes various improvements to the UI, functionality, and
-            features.
-          </small>
-          <br />
-          <p>
-            Skills: React, Javascript, HTML, CSS (MaterialsUI), WeatherAPI.com
-          </p>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/alux444/weather-remastered"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://alux444.github.io/weather-remastered/"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              Link
-            </a>
-          </div>
-        </article>
+                <article className="portfolio_item item">
+                    <div className="portfolio_item-image">
+                        <img src={weatherApp2} alt="weather2" />
+                    </div>
+                    <h3>Weather App V2</h3>
+                    <small>
+                        This is a revisited version of my first project (weather
+                        app). It includes various improvements to the UI,
+                        functionality, and features.
+                    </small>
+                    <br />
+                    <p>
+                        Skills: React, Javascript, HTML, CSS (MaterialsUI),
+                        WeatherAPI.com
+                    </p>
+                    <div className="portfolio_item-cta">
+                        <a
+                            href="https://github.com/alux444/weather-remastered"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            href="https://alux444.github.io/weather-remastered/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            Link
+                        </a>
+                    </div>
+                </article>
 
-        <article className="portfolio_item item">
-          <div className="portfolio_item-image">
-            <img src={FitnessApp} alt="websiteproj"></img>
-          </div>
-          <h3>Fitness Site Project</h3>
-          <small>
-            A react webapp which calls to an exercise API, displaying suggested
-            exercises and calls from a youtube search API to recommend videos.
-            This project was built in a team of 2
-          </small>
-          <br />
-          <p>
-            Skills : React, JavaScript, CSS, HTML, ExerciseDB/YoutubeSearch API
-          </p>
-          <div className="portfolio_item-cta">
-            <a
-              href="https://github.com/alux444/project-fitness"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://alux444.github.io/project-fitness"
-              target="_blank"
-              rel="noreferrer"
-              className="btn"
-            >
-              Link
-            </a>
-          </div>
-        </article>
-      </div>
-    </section>
-  );
+                <article className="portfolio_item item">
+                    <div className="portfolio_item-image">
+                        <img src={FitnessApp} alt="websiteproj"></img>
+                    </div>
+                    <h3>Fitness Site Project</h3>
+                    <small>
+                        A react webapp which calls to an exercise API,
+                        displaying suggested exercises and calls from a youtube
+                        search API to recommend videos. This project was built
+                        in a team of 2
+                    </small>
+                    <br />
+                    <p>
+                        Skills : React, JavaScript, CSS, HTML,
+                        ExerciseDB/YoutubeSearch API
+                    </p>
+                    <div className="portfolio_item-cta">
+                        <a
+                            href="https://github.com/alux444/project-fitness"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            href="https://alux444.github.io/project-fitness"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            Link
+                        </a>
+                    </div>
+                </article>
+
+                <article className="portfolio_item item">
+                    <div className="portfolio_item-image">
+                        <img src={trackTrekker} alt="weather2" />
+                    </div>
+                    <h2>Im currently working on...</h2>
+                    <h3>TrackTrekker</h3>
+                    <small>
+                        An app which recommends songs based off a selected
+                        criteria, using the Spotify API.
+                    </small>
+                    <br />
+                    <div className="portfolio_item-cta">
+                        <a
+                            href="https://github.com/alux444/tracktrekker"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            GitHub
+                        </a>
+                        <a
+                            href="https://alux444.github.io/tracktrekker/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn"
+                        >
+                            Link
+                        </a>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
 };
 
 export default Projects;
