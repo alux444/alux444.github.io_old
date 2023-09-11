9a0a125ec175e727a0b351908b1f2e917423d3cd
alux444 <aluux999@gmail.com>
2023-09-12 11:41:18 +1200

    updates

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import CV from '../../img/cv.pdf'
+import React from "react";
+import CV from "../../img/cv.pdf";
 
 const CTA = () => {
-  return (
-    <div className="cta">
-        <a href={CV} download className="btn">Download CV</a>
-        <a href="#contact" className="btn btn-primary">Contact Me</a>
-    </div>
-  )
-}
+    return (
+        <div className="cta">
+            <a href={CV} download="alex-liang-cv.pdf" className="btn">
+                Download CV
+            </a>
+            <a href="#contact" className="btn btn-primary">
+                Contact Me
+            </a>
+        </div>
+    );
+};
 
-export default CTA
+export default CTA;
