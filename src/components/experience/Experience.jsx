90d880d7acb0851cece12550c3aa4448851ad0e9
alux444 <aluux999@gmail.com>
2023-09-23 22:08:17 +1200

    experience section

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <section id="experience">
+            <h2>My Experience</h2>
+            <div className="ab_container about_paragraph item">
+                <p>Test Automation Engineering Intern</p>
+            </div>
+        </section>
+    );
+};
+
+export default Experience;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,29 +5,29 @@ import ME from "../../img/me.jpg";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return (
-    <header>
-      <div
-        className="container header_container item"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <h5>Hey, I'm</h5>
-        <h1>Alex Liang</h1>
-        <h5 className="text-light">Software Engineering Student</h5>
-        <h5 className="text-light">at the University of Auckland</h5>
-        <CTA></CTA>
-        <div className="me">
-          <img src={ME} alt="me"></img>
-        </div>
-        <HeaderSocials />
-      </div>
-    </header>
-  );
+    return (
+        <header>
+            <div
+                className="container header_container item"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <h5>Hey, I'm</h5>
+                <h1>Alex Liang</h1>
+                <h5 className="text-light">Software Engineering Student</h5>
+                <h5 className="text-light">at the University of Auckland</h5>
+                <CTA></CTA>
+                <div className="me">
+                    <img src={ME} alt="me"></img>
+                </div>
+                <HeaderSocials />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
