9fcdfa13bbbbd90a35a5b47e5eb547ad13ee169e
alux444 <aluux999@gmail.com>
2024-02-23 16:43:58 +1300

    updates

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,10 +6,15 @@ const Experience = () => {
             <h2>My Experience</h2>
             <div className="ab_container about_paragraph item">
                 <h2>Spark NZ</h2>
-                <b>Software Engineering Intern - Test Automation</b>
-                <small>November 2023 - Present</small>
-                <small>• Worked alongside a cross-functional team on the ERP Test Automation Project</small>
-                <small>• Collaborated with QA engineers and manual testers to automate test cases.</small>
+                <b>Software Engineer</b>
+                <small>Feb 2023 - Present</small>
+                <small>• Continued work in the ERP Test Automation Squad</small>
+                <br/>
+                <b>Software Engineering Intern</b>
+                <small>November 2023 - Feb 2023</small>
+                <small>• Delivered over 40 automated test scripts to production</small>
+                <small>• Designed and created a Microsoft PowerApp to display test run artifacts</small>
+                <small>• Collaborated with QA engineers and manual testers to automate test cases</small>
                 <small>• Designed and managed test data on Microsoft SQL Server</small>
             </div>
         </section>
